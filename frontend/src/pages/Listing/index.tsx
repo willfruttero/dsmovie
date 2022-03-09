efcd81405b4b19f9eaa85d44d12534b862ea9674
willfruttero <willfruttero@outlook.com>
2022-03-09 10:25:33 -0300

    Security config

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -7,25 +7,25 @@ function Listing() {
             <Pagination />
             <div className="container">
                 <div className="row">
-                    <div className="col-sm-6 col=lg-4 col-xl-3 mb-3">
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
                         <MovieCard />
                     </div> 
-                    <div className="col-sm-6 col=lg-4 col-xl-3 mb-3">
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
                         <MovieCard />
                     </div> 
-                    <div className="col-sm-6 col=lg-4 col-xl-3 mb-3">
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
                         <MovieCard />
                     </div> 
-                    <div className="col-sm-6 col=lg-4 col-xl-3 mb-3">
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
                         <MovieCard />
                     </div> 
-                    <div className="col-sm-6 col=lg-4 col-xl-3 mb-3">
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
                         <MovieCard />
                     </div> 
-                    <div className="col-sm-6 col=lg-4 col-xl-3 mb-3">
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
                         <MovieCard />
                     </div> 
-                    <div className="col-sm-6 col=lg-4 col-xl-3 mb-3">
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
                         <MovieCard />
                     </div>                   
                 </div>
